eaeafed062a93c01e24246a04a44d060e5c0f0da
Vasilii Malai <malaivasili@gmail.com>
2025-07-14 02:17:54 +0300

    refactor: modified functional component to class component

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return <></>;
-}
+import Header from './components/Header';
+import Main from './components/Main';
+import { Component } from 'react';
 
-export default App;
+type StateParams = {
+  searchQuery: string | null;
+};
+
+export default class App extends Component {
+  state: StateParams = {
+    searchQuery: localStorage.length
+      ? localStorage.getItem('searchQuery')
+      : null,
+  };
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Main />
+      </>
+    );
+  }
+}
