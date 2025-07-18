d255d7ff415a3f49590cb597bf4fea62b443da42
Vasilii Malai <malaivasili@gmail.com>
2025-07-18 20:14:43 +0300

    refactor: move code from App to Main component

--- rs-react-app/src/components/Main.tsx
+++ rs-react-app/src/components/Main.tsx
@@ -1,4 +1,36 @@
-const Main = () => {
-  return <div>Main</div>;
+import Search from './Search';
+import { Component } from 'react';
+import getData from '.././features/get-data';
+
+type StateParams = {
+  searchQuery: string | null;
 };
-export default Main;
+
+export default class Main extends Component {
+  state: StateParams = {
+    searchQuery: localStorage.getItem('searchQuery') || null,
+  };
+
+  handleStateChange = (value: string) => {
+    this.setState({
+      searchQuery: value,
+    });
+  };
+
+  componentDidMount(): void {
+    getData();
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <Search
+            searchQuery={this.state.searchQuery || ''}
+            onButtonClick={this.handleStateChange}
+          />
+        </header>
+      </>
+    );
+  }
+}
