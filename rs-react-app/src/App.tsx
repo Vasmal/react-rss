5f9fab12616e01203cbd1d69b575ef6e9e8c208a
Vasilii Malai <malaivasili@gmail.com>
2025-07-18 20:16:05 +0300

    refactor: remove logical code, paste Main.tsx

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,33 +1,9 @@
-import Header from './components/Header';
 import Main from './components/Main';
-import { Component } from 'react';
 
-type StateParams = {
-  searchQuery: string | null;
-};
-
-export default class App extends Component {
-  state: StateParams = {
-    searchQuery: localStorage.length
-      ? localStorage.getItem('searchQuery')
-      : null,
-  };
-
-  handleStateChange = (searchValue: string) => {
-    this.setState({
-      searchQuery: searchValue,
-    });
-  };
-
-  render() {
-    return (
-      <>
-        <Header
-          searchQuery={this.state.searchQuery ? this.state.searchQuery : ''}
-          handleStateChange={this.handleStateChange}
-        />
-        <Main />
-      </>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
