7afadee04cfc922b6d3e6f7607234d816ae843c5
Vasilii Malai <malaivasili@gmail.com>
2025-07-20 04:34:23 +0300

    feat: move Main to folder Main

---
+++ rs-react-app/src/components/Main/Main.tsx
@@ -0,0 +1,57 @@
+import Search from '../Search/Search';
+import { Component } from 'react';
+import CardList from '../CardList';
+import ErrorBoundary from '../ErrorBoundary';
+import styles from './Main.module.css';
+
+export type RepoParams = {
+  id: number;
+  full_name: string;
+  clone_url: string;
+  description: string;
+  homepage?: string;
+  language?: string;
+  stargazers_count: number;
+};
+
+type StateParams = {
+  searchQuery: string | null;
+};
+
+export default class Main extends Component {
+  state: StateParams = {
+    searchQuery: localStorage.getItem('searchQuery') || null,
+  };
+
+  handleStateChange = (value: string) => {
+    this.setState({
+      searchQuery: value,
+    });
+  };
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <header>
+          <h1>Here you can find any repo from the GitHub.</h1>
+          <p>
+            Search repositories by name or topic and select your preferred one
+          </p>
+          <p className={styles.search_examples}>
+            {`Try: 'javascript framework', 'python bot', 'mobile game', or any tech
+          stack`}
+          </p>
+          <Search
+            searchQuery={this.state.searchQuery || ''}
+            onButtonClick={this.handleStateChange}
+          />
+        </header>
+        <main>
+          <ErrorBoundary>
+            <CardList searchQuery={this.state.searchQuery || ''} />
+          </ErrorBoundary>
+        </main>
+      </div>
+    );
+  }
+}
