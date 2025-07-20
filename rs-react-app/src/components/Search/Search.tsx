7fe62853e9d4299255df1422cf91492ed1dd0566
Vasilii Malai <malaivasili@gmail.com>
2025-07-20 04:35:18 +0300

    refactor: move Search to folder Search

---
+++ rs-react-app/src/components/Search/Search.tsx
@@ -0,0 +1,50 @@
+import { Component } from 'react';
+import styles from './Search.module.css';
+
+type Props = {
+  searchQuery: string;
+  onButtonClick: (value: string) => void;
+};
+
+type State = {
+  input: string;
+};
+
+class Search extends Component<Props, State> {
+  state: State = {
+    input: this.props.searchQuery,
+  };
+
+  handleClick = (value: string) => {
+    if (!value) {
+      return;
+    }
+    const trimedValue = value.trim();
+    this.props.onButtonClick(trimedValue);
+    if (trimedValue !== localStorage.getItem('searchQuery')) {
+      localStorage.setItem('searchQuery', trimedValue);
+    }
+  };
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <input
+          className={styles.search_field}
+          type="text"
+          placeholder="Search repositories..."
+          value={this.state.input}
+          onChange={(e) => this.setState({ input: e.target.value })}
+        />
+        <button
+          className={styles.search_button}
+          onClick={() => this.handleClick(this.state.input)}
+        >
+          Add
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Search;
