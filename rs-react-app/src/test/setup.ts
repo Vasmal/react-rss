dc0b8972a2c3e48ead5627d56ffb3cfe52f8fabd
Vasilii Malai <malaivasili@gmail.com>
2025-07-28 02:53:06 +0300

    refactor: remove comments

--- rs-react-app/src/test/setup.ts
+++ rs-react-app/src/test/setup.ts
@@ -2,7 +2,6 @@ import { expect, afterEach, beforeAll, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
 import '@testing-library/jest-dom/vitest';
-// import '@testing-library/jest-dom';
 
 expect.extend(matchers);
 
@@ -11,37 +10,25 @@ afterEach(() => {
 });
 
 beforeAll(() => {
-  // Object.defineProperty(window, 'localStorage', {
-  //   value: {
-  //     getItem: vi.fn(() => null),
-  //     setItem: vi.fn(() => null),
-  //     removeItem: vi.fn(() => null),
-  //     clear: vi.fn(() => null),
-  //   },
-  // });
-  // Настройка jsdom окружения
   Object.defineProperty(window, 'matchMedia', {
     writable: true,
     value: vi.fn().mockImplementation((query) => ({
-      matches: false, // CSS media query не совпадает
-      media: query, // Исходный запрос
-      onchange: null, // Callback для изменений
-      addListener: vi.fn(), // Deprecated
-      removeListener: vi.fn(), // Deprecated
-      addEventListener: vi.fn(), // Современный способ
-      removeEventListener: vi.fn(), // Современный способ
-      dispatchEvent: vi.fn(), // Генерация событий
+      matches: false,
+      media: query,
+      onchange: null,
+
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
     })),
   });
 
-  // Мок для IntersectionObserver (часто нужен для компонентов)
   globalThis.IntersectionObserver = vi.fn().mockImplementation(() => ({
     observe: vi.fn(),
     disconnect: vi.fn(),
     unobserve: vi.fn(),
   }));
 
-  // Мок для ResizeObserver
   globalThis.ResizeObserver = vi.fn().mockImplementation(() => ({
     observe: vi.fn(),
     disconnect: vi.fn(),
